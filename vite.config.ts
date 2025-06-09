a1534227502a3feadeb78591f589ee7cef9460a1
nhokanhanh@gmail.com <nhokanhanh@gmail.com>
2025-06-09 21:57:03 +0700

    first project

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vitest/config"
+import react from "@vitejs/plugin-react"
+
+import dns from 'dns'
+// https://vitejs.dev/config/server-options.html#server-options
+//if u want to run as 127.0.0.1, comment the next line below
+dns.setDefaultResultOrder('verbatim')
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true,
+    port: 3000
+  },
+  build: {
+    outDir: "build",
+    sourcemap: true,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "src/setupTests",
+    mockReset: true,
+  },
+})
